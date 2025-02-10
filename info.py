e8bedf18122d97fdf8e61d61ebe8afded0769b1e
photon256 <144157217+photon256@users.noreply.github.com>
2025-02-10 18:09:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dildaaryaara:dildarcluster01@cluster0.hpxj6.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dildaar:DF9mb6XfI2QUy57K@cluster0.2orwy.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "abx")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
